2c00cddc107764afd26bb27740f42f3f17d87b8e
Joey Benenati <joey.benenati@gmail.com>
2016-01-18 22:37:05 -0600

    added active state toggle to trip navbar

--- client/components/trip/Trip.jsx
+++ client/components/trip/Trip.jsx
@@ -1,35 +1,54 @@
 Trip = React.createClass({
 
   renderHome: function () {
+    $('.active').removeClass('active');
+    $('#home').addClass('active');
     ReactDOM.render(<TripHome/>, document.getElementById('trip-module'))
   },
 
   renderItinerary: function () {
+    $('.active').removeClass('active');
+    $('#itinerary').addClass('active');
     ReactDOM.render(<Itinerary/>, document.getElementById('trip-module'))
+  },
+
+  renderChat: function () {
+    $('.active').removeClass('active');
+    $('#chat').addClass('active');
+  },
+
+  renderSettings: function () {
+    $('.active').removeClass('active');
+    $('#settings').addClass('active');
+  },
+
+  renderExpenses: function () {
+    $('.active').removeClass('active');
+    $('#cash').addClass('active');
   },
 
 	render: function(){
     return (
       <div>
         <div className="tabs tabs-icon-top">
-          <a className="tab-item" onClick={this.renderHome}>
+          <a className="tab-item active" id='home'onClick={this.renderHome}>
             <i className="icon ion-home"></i>
             Home
           </a>
-          <a className="tab-item" onClick={this.renderItinerary}>
+          <a className="tab-item" id='itinerary' onClick={this.renderItinerary}>
             <i className="icon ion-ios-list-outline"></i>
             Itinerary
           </a>
-          <a className="tab-item">
+          <a className="tab-item" id="chat" onClick={this.renderChat}>
             <i className="icon ion-chatboxes"></i>
             Chat
           </a>
-          <a className="tab-item">
-            <i className="icon ion-cash"></i>
+          <a className="tab-item" id='cash' onClick={this.renderExpenses}>
+            <i className="icon ion-cash expenses"></i>
             Expenses
           </a>
-          <a className="tab-item">
-            <i className="icon ion-gear-a"></i>
+          <a className="tab-item" id='settings' onClick={this.renderSettings}>
+            <i className="icon ion-gear-a settings"></i>
             Settings
           </a>
         </div>
