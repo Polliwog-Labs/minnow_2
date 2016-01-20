b1cc9184f2475ee2d755ae9376bc3e0984236332
Ryan Hamblin <ryanhca@gmail.com>
2016-01-20 16:11:05 -0600

    just chuggin a long

--- client/components/messages/MessageLoader.jsx
+++ client/components/messages/MessageLoader.jsx
@@ -1,5 +1,5 @@
 MessageLoader = React.createClass({
-	render(){
+	renderMessages(){
 		var params = {
 			text: '',
 			created_at: '',
@@ -8,13 +8,22 @@ MessageLoader = React.createClass({
 		for(var key in this.props.messages){
 			this.props.messages[key] && (params[key] = this.props.messages[key]);
 		};
-		console.log('Params!!!: ', params);
+		params.messages.map(function(message){
+			var message_text = message.text;
+			console.log("message_text: ", message_text)
+			var sender_name = message.sender;
+			console.log("sender_name: ", sender_name)
+		})
+	},
+	render(){
+
 		return(
 			<div className="list message-container">
+			{this.renderMessages()}
 				<a className='anchor-resize item item-avatar'>
 					<img className="avatar-image" src="https://facebook.github.io/react/img/logo.svg" />
-					<h2>{params}</h2>
-					<p>{params}</p>
+					<h2>{this.sender_name}</h2>
+					<p>{this.message_text}</p>
 				</a>
 			</div>
 		)
