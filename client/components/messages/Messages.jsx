a09fd65f1bf645c4096094f67673125dcb81ab90
Daniel Sato <dksato@gmail.com>
2016-01-21 15:06:13 -0600

    Using meteor data for message refreshing.

--- client/components/messages/Messages.jsx
+++ client/components/messages/Messages.jsx
@@ -1,9 +1,16 @@
 Messages = React.createClass({
-	
+	propTypes: {
+		trip: React.PropTypes.object.isRequired
+	},
+	mixins: [ReactMeteorData],
+	getMeteorData(){
+		var trip = Trips.findOne({_id:this.props.trip._id});
+		return {trip:trip}
+	},
 	submitMessage(event){
 		event.preventDefault();
 		var message = ReactDOM.findDOMNode(this.refs.message_text).value;
-		Trips.update({"_id": this.props.trip._id}, {$push: {'messages': {'text': message, 'created_at': new Date(), 'sender': Meteor.user().username}}}, function(error){
+		Trips.update({_id:this.data.trip._id}, {$push: {'messages': {'text': message, 'created_at': new Date(), 'sender': Meteor.user().username}}}, (error)=>{
 			if(!error){
 				console.log("inserted message into DB")
 			}else if(error){
@@ -11,11 +18,10 @@ Messages = React.createClass({
 			}
 		});
 	},
-
 	render(){
 		return(
 			<div className="message-wrapper">
-			<MessageLoader messages={this.props.trip}/>
+			<MessageLoader messages={this.data.trip}/>
 			<div className='list fixed-input'>
 				<form className='item item-input-inset'>
 					<label className='item-input-wrapper'>
