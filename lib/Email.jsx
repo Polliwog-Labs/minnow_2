e98d9699758d376409a4c97d033fb0e577c8a7fb
Daniel Sato <dksato@gmail.com>
2016-01-23 17:00:48 -0600

    Censored Email PW

--- lib/Email.jsx
+++ lib/Email.jsx
@@ -1,3 +1,3 @@
 if (Meteor.isServer){
-  process.env.MAIL_URL='smtp://team.polliwog@gmail.com:aajbmerhds@smtp.gmail.com:465'
+  process.env.MAIL_URL='smtp://team.polliwog@gmail.com:PASSWORD@smtp.gmail.com:465'
 }
