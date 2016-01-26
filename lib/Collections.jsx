d9ab984bb67f71882f8683f4a5f9b0ae68c4f0fa
Daniel Sato <dksato@gmail.com>
2016-01-26 12:27:54 -0600

    Added Profile pic collections + methods

--- lib/Collections.jsx
+++ lib/Collections.jsx
@@ -1,3 +1,4 @@
+//Methods
 var resizeImage = function(fileObj, readStream, writeStream) {
   gm(readStream, fileObj.name()).resize(800,600).stream().pipe(writeStream);
 };
@@ -20,6 +21,11 @@ var backgroundImage = function(fileObj, readStream, writeStream) {
   gm(readStream, fileObj.name()).resize(1000,null).gravity('Center').crop(1000,200).stream().pipe(writeStream);
 };
 
+var profileImageResize = function(fileObj, readStream, writeStream) {
+  gm(readStream, fileObj.name()).resize(120,120).gravity('Center').crop(80,80).stream().pipe(writeStream);
+};
+
+//Stores
 var imageStore = new FS.Store.GridFS("images",{
   beforeWrite: renameImage,
   transformWrite: resizeImage
@@ -30,6 +36,12 @@ var BGStore = new FS.Store.GridFS("backgrounds",{
   transformWrite: backgroundImage
 });
 
+var profileStore = new FS.Store.GridFS("profilepics",{
+  beforeWrite: renameImage ,
+  transformWrite: profileImageResize
+});
+
+//Collections
 Images = new FS.Collection("images",{
   stores: [imageStore,BGStore],
   filter: {
@@ -46,7 +58,32 @@ Images = new FS.Collection("images",{
     }
   }
 });
+
 Images.allow({
+  download(){return true},
+  insert(){return true},
+  update(){return true},
+  remove(){return true}
+});
+
+ProfilePics = new FS.Collection("profilepics",{
+  stores: [profileStore],
+  filter: {
+    allow: {
+      contentTypes: ['image/*'],
+    },
+    deny: {
+      extensions: ['gif']
+    },
+    onInvalid(){
+      if (Meteor.isClient){
+        alert('this is a bullshit image');
+      }
+    }
+  }
+});
+
+ProfilePics.allow({
   download(){return true},
   insert(){return true},
   update(){return true},
