5d94607bf914ec06dc085a8fa4aaf01f505ec32f
Ryan Hamblin <ryanhca@gmail.com>
2016-01-26 19:12:27 -0600

    fleshing out accounts

--- client/components/accounts/Account.jsx
+++ client/components/accounts/Account.jsx
@@ -4,13 +4,21 @@ Account = React.createClass({
 
 	getMeteorData(){
 		 var user = Meteor.user();
-		 
-		 return {};
+		 return {user};
 	},
 	render(){
 		return(
 			<div>
-				<h2>Hello {user.username}</h2>
+			<ReactBootstrap.Grid>
+				<ReactBootstrap.Row>
+					<ReactBootstrap.Col xs={6} md={4}>
+						<ReactBootstrap.Image src='/prof-placeholder.jpg' circle/>
+					</ReactBootstrap.Col>
+				</ReactBootstrap.Row>
+			</ReactBootstrap.Grid>
+				<h2>Hello {Meteor.user().username}</h2>
+				<h2>{Meteor.user().emails[0].address}</h2>
+				<h4>Recent Trip Invites {Meteor.user().profile.invites.length}</h4>
 			</div>
 		)
 	}
