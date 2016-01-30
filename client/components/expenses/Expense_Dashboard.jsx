8592cd64fd14f765597ae3d9b01351ecd1daaaa9
Mac Evans <maclean.evans@gmail.com>
2016-01-30 15:23:11 -0600

    finished expenses

--- client/components/expenses/Expense_Dashboard.jsx
+++ client/components/expenses/Expense_Dashboard.jsx
@@ -4,9 +4,16 @@ ExpenseDashboard = React.createClass({
 		var key = Object.keys(member);
 		var setUp = this.props[key];
 		var balance = Number((setUp).toFixed(2));
+
+
+
 		return (
 			<div className='item item-divider header-expenses'>
-				<p className='balance'>Balance with {key} is ${balance}</p>
+				{ balance === 0 ? <p className='balance'>You are even with {key}</p>:
+				    balance > 0 ? 
+					<p className='balance'>{key} owes you ${balance}</p> : 
+					<p className='balance'>You owe {key} ${(balance) * -1}</p>
+				}
 			</div>
 
 		)
