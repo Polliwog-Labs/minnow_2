72e5b3821a05e18037c3098684cb76eeac61f39d
Joey Benenati <joey.benenati@gmail.com>
2016-02-02 17:23:29 -0600

    added VenmoButton component

--- client/components/trip/components/Itinerary/components/ItineraryEvent.jsx
+++ client/components/trip/components/Itinerary/components/ItineraryEvent.jsx
@@ -15,6 +15,10 @@ ItineraryEvent = React.createClass({
     })
   },
 
+  navToLink() {
+    window.open(this.props.idea.og.url, '_system')
+  },
+
   render() {
     this.props.event.og = this.props.event.og || {image:"http://static1.squarespace.com/static/5542dcefe4b0f37cdc4d6e60/5542e8dae4b0b79fc2fc8dfd/5542e9a0e4b0b79fc2fc9a9e/1430448712022/Virginia-Group-Travel1.jpeg",
     title:'',description:'', url: ''}
@@ -24,7 +28,7 @@ ItineraryEvent = React.createClass({
           <h2>{this.displayTime().toString()} </h2>
           <h3>{this.props.event.name}</h3>
           <p>{this.props.event.desc}</p>
-          <a href={this.props.event.og.url} target="_blank"><h3 className='event-link'>{this.props.event.og.title}</h3></a>         
+          <a  onClick={this.navToLink} ><h3 className='event-link'>{this.props.event.og.title}</h3></a>         
           <p>{this.props.event.og.description}</p>
           <div className='row'>
             <div className='col-xs-2'>
