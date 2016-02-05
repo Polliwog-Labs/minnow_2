db0969221a3aab843f84356b37afb10dffee2384
Daniel Sato <dksato@gmail.com>
2016-02-04 18:55:26 -0600

    Added camera support to Account, Photos, editTrip

--- client/AbsUrl.js
+++ client/AbsUrl.js
@@ -1,7 +1,7 @@
-AbsUrl = 'http://www.minnow.pw';
+// AbsUrl = 'http://www.minnow.pw';
 
 // AbsUrl='http://localhost:3000'
-// AbsUrl='http://192.168.1.191:3000'
+AbsUrl='http://192.168.1.191:3000'
 //AbsUrl='http://192.168.1.223:3000'
 
 // AbsUrl='http://192.168.1.115:3000'
